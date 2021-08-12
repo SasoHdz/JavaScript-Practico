a5b1766cd18a90512ed7a2ac154bc0ec73a56c93
= <=>
2021-08-11 23:37:20 -0500

    Formulario de cuadrado creado

--- figuras.js
+++ figuras.js
@@ -1,38 +1,13 @@
 // Area Cuadrado
-console.group("Cuadrados");
-//const ladoCuadrado = 5;
-//console.log("Los lados del cuadrado miden: "+ ladoCuadrado + "cm");
-
 function perimetroCuadrado(lado) {
     return lado * 4;
 }
-//console.log("El perimetro mide: "+ perimetroCuadrado + "cm");
 
 function areaCuadrado (lado){
     return lado*lado;
 }
-//console.log("El area mide : "+ areaCuadrado + "cm^2");
-
-console.groupEnd();
 
 // Area Triangulo
-console.group("Triangulos");
-/* const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-console.log(
-    "Los lados del triangulo miden: "
-    + ladoTriangulo1 
-    + "cm, "
-    + ladoTriangulo2 
-    + "cm, " 
-    + baseTriangulo 
-    + "cm"
-); */
-
-/* const alturaTriangulo = 5.5;
-console.log("La altura del triángulo es :"+ alturaTriangulo + "cm"); */
-
 function perimetroTriangulo(lado1, lado2, base)
 {
     return lado1+lado2+base;
@@ -42,16 +17,7 @@ function areaTriangulo(base, altura) {
     return (base * altura)/2;
 }
 
-
-console.groupEnd();
-
 // Area Circulo
-console.group("Circulos");
-
-//Radio
-/* const radioCirculo = 4;
-console.log("El radio del circulo es: "+ radioCirculo+"cm"); */
-
 //Diametro
 function diametroCirculo(radio){
     return radio * 2
@@ -72,6 +38,23 @@ function areaCirculo(radio){
     return PI * (radio * radio);
 }
 
+// Interactuando con el HTML
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
 
+    const perimetro = perimetroCuadrado(value);
 
-console.groupEnd();
+    alert(perimetro);
+
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+
+    alert(area);
+    
+}
