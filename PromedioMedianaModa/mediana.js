8ef3c8f3f049c2f5d0cc742577c81213a9a580a6
= <=>
2021-08-17 14:24:03 -0500

    Formula de mediana encapsulada

---
+++ PromedioMedianaModa/mediana.js
@@ -0,0 +1,33 @@
+/* const lista1 = [
+    6,
+    2,
+    4,
+    3,
+    1,
+]; */
+
+function esPar(num) {
+    if(num % 2 === 0) return true;
+    else return false;
+}
+
+function calcularMediana(lista)
+{
+    lista.sort(); 
+
+    const mitadLista = parseInt(lista.length / 2);
+
+    let mediana;
+
+    if (esPar(lista.length)) {
+        const element1 = lista[mitadLista];
+        const element2 = lista[mitadLista-1];
+
+        mediana =  (element1 + element2)/2;
+    }
+    else {
+        mediana = lista[mitadLista];
+    }
+
+    return mediana;
+}
